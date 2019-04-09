392f751dca16740e4d6e62a6dccd63d52fb75611
iste-lab1 <iste-lab1@istelab1-HP>
2019-04-09 19:55:59 +0300

    JavaFX- Mysql Tableview Veri cekme

--- JavaFX_Mysql_Login/src/application/SampleController.java
+++ JavaFX_Mysql_Login/src/application/SampleController.java
@@ -10,6 +10,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
 import java.sql.*;
 
 
